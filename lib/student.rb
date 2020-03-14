9c2e3491a3f22e8f2d211b9b59ab66d312646f3c
Dakota States <dakota.states@gmail.com>
2020-03-14 21:13:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,18 +11,16 @@ class Student < InteractiveRecord
     DB[:conn].results_as_hash = true
     
     sql = "PRAGMA table_info('#{table_name}')"
-    table_info = DB[:conn].execute(sql)
-    column_names = []
     
-    table_info.each do |column|
-      column_names << column["name"]
-    end
-    column_names.compact
+    table_info = DB[:conn]
   end
+  
   
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
+  
+  
 end
